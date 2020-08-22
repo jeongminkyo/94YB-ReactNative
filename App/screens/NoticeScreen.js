bf2c53c0c2150ca3f99fe9781650d60b6fd87ccb
Jeong Minkyo <redwonder@naver.com>
2020-08-22 19:05:40 +0900

    issue#13 : notice api 연동

--- App/screens/NoticeScreen.js
+++ App/screens/NoticeScreen.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     View,
-    Text,
     StyleSheet,
     ScrollView,
     RefreshControl
@@ -13,97 +12,36 @@ import Items from '../data/pictures';
 
 import { isIphoneX, getBottomSpace } from 'react-native-iphone-x-helper';
 
-const posts = [
-    {
-    "id": 34,
-    "title": "2020년 1월 동계휴가 결산보고 ",
-    "context": "https://docs.google.com/spreadsheets/d/1diAIj8hsVQh8sfdVUCjyAxgOVYM0XfILu36ZHloUM6k/edit#gid=0",
-    "user_id": 1,
-    "created_at": "2020-01-06 21:25:15",
-    "display_name": "관리자",
-    "attachments": [
-                        {
-                        "id": 77,
-                        "url": "https://94yb.s3.amazonaws.com/uploads/notice_attachment/s3/77/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2020-01-06_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_9.24.12.png"
-                        },
-                        {
-                        "id": 78,
-                        "url": "https://94yb.s3.amazonaws.com/uploads/notice_attachment/s3/78/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2020-01-06_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_9.24.24.png"
-                        }
-    ],
-    "comments": [
-            {
-            "id": 3,
-            "content": "댓글 1\r\n",
-            "display_name": "관리자",
-            "created_at": "2020-08-20 23:11:40"
-            },
-            {
-            "id": 4,
-            "content": "댓글 2",
-            "display_name": "관리자",
-            "created_at": "2020-08-20 23:11:44"
-            }
-        ]
-    },
-    {
-    "id": 33,
-    "title": "2020년 술친게이들 동계 휴가 공지",
-    "context": "일시 : 2020.01.04(토)  10시\r\n장소 : 양북 미니스톱 편의점\r\n숙소 : 경주 블루원 리조트",
-    "user_id": 1,
-    "created_at": "2020-01-02 12:09:41",
-    "display_name": "관리자",
-    "attachments": [
-                        {
-                        "id": 74,
-                        "url": "https://94yb.s3.amazonaws.com/uploads/notice_attachment/s3/74/s0203_gallery_img01.jpg"
-                        },
-                        {
-                        "id": 75,
-                        "url": "https://94yb.s3.amazonaws.com/uploads/notice_attachment/s3/75/s0203_gallery_img02.jpg"
-                        },
-                        {
-                        "id": 76,
-                        "url": "https://94yb.s3.amazonaws.com/uploads/notice_attachment/s3/76/s0203_gallery_img03.jpg"
-                        }
-    ],
-    "comments": []
-    },
-    {
-    "id": 28,
-    "title": "2019년 7월 하계휴가 결산보고",
-    "context": "https://docs.google.com/spreadsheets/d/1mHM41SU62gIZ1AR1Y-CbCphlSmgkmlbgYSHfuE7QbA4/edit#gid=0",
-    "user_id": 1,
-    "created_at": "2019-08-07 05:21:42",
-    "display_name": "관리자",
-    "attachments": [
-                        {
-                        "id": 72,
-                        "url": "https://94yb.s3.amazonaws.com/uploads/notice_attachment/s3/72/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2019-08-07_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_2.20.56.png"
-                        },
-                        {
-                        "id": 73,
-                        "url": "https://94yb.s3.amazonaws.com/uploads/notice_attachment/s3/73/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2019-08-07_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_2.17.29.png"
-                        }
-    ],
-    "comments": []
-    }
-]
-
-const wait = (timeout) => {
-    return new Promise(resolve => {
-      setTimeout(resolve, timeout);
-    });
-}
-
 const NoticeScreen = () => {
 
     const [refreshing, setRefreshing] = React.useState(false);
+    const [posts, setPosts] = React.useState([]);
 
     const onRefresh = React.useCallback(() => {
         setRefreshing(true);
 
-        wait(2000).then(() => setRefreshing(false));
+        getPostInfo(1).then(res => setRefreshing(false));
+    }, []);
+
+    const getPostInfo = async (page) => {
+        try {
+            const res = await fetch(`https://yb94.name/api/v1/notices?${page}`);
+            const json = await res.json();
+            setPosts(json.notices);
+        }
+        catch (error) {
+            showError('알 수 없는 에러가 발생했습니다.');
+        }
+    }
+
+    const showError = (message) => {
+        setTimeout(() => {
+            Alert.alert(message)
+        }, 500)
+    }
+
+    useEffect(() => {
+        getPostInfo(1);
     }, []);
 
     return (
